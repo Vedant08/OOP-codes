ef1dbb56339c803f395022d27e49739fef57dbd7
Vedant08 <55072510+Vedant08@users.noreply.github.com>
2019-12-03 10:01:00 +0530

    Update Ass3_21810949.cpp

--- Ass3_21810949.cpp
+++ Ass3_21810949.cpp
@@ -7,87 +7,63 @@ Area of rectangle= x*y Area of triangle =1/2*x*y
 #include<iostream>
 using namespace std;
 
-class Shape
+class shape
 {
+		
 	public:
-        double length,width;
-        void get_data() //Accepting values
-        {
-        cout<<"Enter the length-->>";
-        cin>>length;
-        cout<<"Enter the width-->>";
-        cin>>width;
-        }
-        void display()  //Displaying values
-        {
-        cout<<"length-->>"<<length;
-        cout<<"\t width-->>"<<width<<"\n";
-        }
+		float a,b;
+	
+		virtual void getdata()=0;
+		virtual void sarea()=0;
 };
 
-class rectangle:public Shape //Inheriting base class shape
+class triangle: public shape
 {
-	public:
-        double result;
-        void cal()    //Calculating  area of rectangle
-        {
-        get_data();
-        display();
-        result=(length*width);
-        }
-        void display_area() //Displaying area of rectangle
-        {
-        cout<<"Area of Rectangle-->>"<<result<<"\n";
-        }
+	void getdata()
+	{
+		
+		cout<<"Enter the Height and Base\n";
+		cin>>a>>b;
+	}
+	
+	void sarea()
+	{
+		cout<<"Area of Triangle: "<<0.5*a*b<<"squnit\n";
+	}
+	
 };
 
-class triangle:public Shape //Inheriting of shape class
+class rectangle: public shape
 {
-	public:
-        double result;
-
-        void cal() //Calculating area of triangle
-        {
-        get_data();
-        display();
-        result=(length*width)/2;
-        }
-        void display_area()  //Displaying area of Triangle
-        {
-        cout<<"Area of Triangle-->>"<<result<<"\n";
-        }
+	void getdata()
+	{
+		
+		cout<<"Enter the Length and B\n";
+		cin>>a>>b;
+	}
+	
+	void sarea()
+	{
+		cout<<"Area of Rectangle: "<<a*b<<"squnit\n";
+	}
+	
 };
+
+
 
 int main()
 {
-	int check;
-	char repeat='n';
-        rectangle rec;
-        triangle tri;
-        do
-        {
-	 cout<<"Pick any one from the given choice:-\n";
-        cout<<"1.Area of Rectangle.\n";
-	cout<<"2.Area of Triangle.\n";
-        cout<<"Enter the choice::";
-        cin>>check;
-        switch(check)  // switch case used for multiple operation
-        {
-		case 1:
-    		rec.cal();
-    		rec.display_area();
-    		break;
-		case 2:
-    		tri.cal();
-    		tri.display_area();
-    		break;
-		default:
-    		cout<<"Enter the valid choice::\n";
-    		break;
-	}
-        cout<<"Do you want to continue (Y/N)::";
-        cin>>repeat;
-        }
-	while(repeat=='Y'||repeat=='y');
-    return 0;
+	
+	triangle tri;
+	shape *stri=&tri;
+	stri->getdata();
+	stri->sarea();
+	
+	rectangle rec;
+	shape *srec=&rec;
+	srec->getdata();
+	srec->sarea();
+	
+
+	return 0;
 }
